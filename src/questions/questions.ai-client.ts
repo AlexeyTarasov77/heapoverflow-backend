c69d65be5d09417d759b78e88a91ed76d0f8448b
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-03-22 17:37:22 +0100

    Created llama api client for generating answer for question

---
+++ src/questions/questions.ai-client.ts
@@ -0,0 +1,28 @@
+import { Question } from './entities/question.entity';
+import { IAnswerGenerator } from './questions.types';
+
+type LlamaResponse = { response: string };
+
+export class LlamaAPIClient implements IAnswerGenerator {
+  private baseUrl: string;
+  private defaultOptions: object;
+  constructor() {
+    this.defaultOptions = {
+      model: 'llama3.2',
+      stream: false,
+    };
+    this.baseUrl = 'http://localhost:11434/api';
+  }
+  generateAnswerForQuestion = async (question: Question): Promise<string> => {
+    const resp = await fetch(this.baseUrl + '/generate', {
+      method: 'POST',
+      body: JSON.stringify({
+        ...this.defaultOptions,
+        prompt: `Answer the question with short informative answer: ${question.title}`,
+      }),
+    });
+    const data: LlamaResponse = await resp.json();
+    console.log("API RESPONSE", data)
+    return data.response;
+  };
+}
